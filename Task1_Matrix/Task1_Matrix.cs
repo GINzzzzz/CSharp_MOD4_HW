0032c50cffa1dab165d18cfa81926080a83e789f
Nikita An <inventstar@rambler.ru>
2023-01-20 12:17:42 +0300

    commit_1: commit task files (task 1 - 3 of the homework for CSharp_Dev.Module4)

---
+++ Task1_Matrix/Task1_Matrix.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите количество строк: ");
+int cntRows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int cntCols = int.Parse(Console.ReadLine());
+
+Random rand = new Random();
+
+int[,] matrix = new int[cntRows, cntCols];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rand.Next(-10, 10);
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine("\t");
+}
---
+++ Task2_MatrixSum/Task2_MatrixSum.cs
@@ -0,0 +1,50 @@
+﻿Console.Write("Введите количество строк: ");
+int cntRows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int cntCols = int.Parse(Console.ReadLine());
+
+Random rand = new Random();
+
+int[,] matrix1 = new int[cntRows, cntCols];
+int[,] matrix2 = new int[cntRows, cntCols];
+int[,] matrixSum = new int[cntRows, cntCols];
+
+Console.WriteLine("\nМатрица 1:\n");
+
+for (int i = 0; i < matrix1.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix1.GetLength(1); j++)
+    {
+        matrix1[i, j] = rand.Next(-10, 10);
+        Console.Write($"{matrix1[i, j]}\t\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nМатрица 2:\n");
+
+for (int i = 0; i < matrix2.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix2.GetLength(1); j++)
+    {
+        matrix2[i, j] = rand.Next(-10, 10);
+        Console.Write($"{matrix2[i, j]}\t\t");
+    }
+    Console.WriteLine();
+}
+
+// Т.к. сложение возможно только в случае одинаковой размерности матрицы для вложенного
+// цикла используем cntRows, cntCols. Однако может использоваться GetLength в отношении
+// любой из матриц.
+
+Console.WriteLine("\nСумма матриц 1 и 2:\n");
+
+for (int i = 0; i < cntRows; i++)
+{
+    for (int j = 0; j < cntCols; j++)
+    {
+        matrixSum[i, j] = matrix1[i, j] + matrix2[i, j];
+        Console.Write($"{matrixSum[i, j]}\t\t");
+    }
+    Console.WriteLine();
+}
